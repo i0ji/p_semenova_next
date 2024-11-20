046dd853860b1b95467e51483800d7e00c6d62f1
i0ji <ioji@inbox.ru>
2024-11-20 16:42:03 +0300

    tag

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,9 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   distDir: 'build',
   output: isProd ? 'export' : undefined,
-  basePath: '',
+  // basePath: '',
+  // assetPrefix: '',
+  basePath: '/p_katerina_semenova_next',
   assetPrefix: '',
   webpack: (config) => {
     config.resolve.alias['@styles'] = './src/styles/*';
