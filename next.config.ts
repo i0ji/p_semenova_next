b4e5c2b7ebafa3fdfe88ee159222d2a88b59c029
i0ji <ioji@inbox.ru>
2024-11-21 16:26:25 +0300

    v13

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
   },
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   basePath: '/p_katerina_semenova_next',
-  assetPrefix: '/',
+  assetPrefix: '.',
   webpack: (config) => {
     config.resolve.alias['@styles'] = './src/styles/*';
     return config;
