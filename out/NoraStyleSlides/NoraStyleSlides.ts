796eaa507004330f7cde01719fb125cfb189d211
i0ji <ioji@inbox.ru>
2024-11-21 16:33:13 +0300

    v16

--- out/NoraStyleSlides/NoraStyleSlides.ts
+++ out/NoraStyleSlides/NoraStyleSlides.ts
@@ -1,6 +1,6 @@
 export const NoraStyleSlides = [
-  { id: 18, img: '/NoraStyleSlides/s1.webp' },
-  { id: 19, img: '/NoraStyleSlides/s2.webp' },
-  { id: 20, img: '/NoraStyleSlides/s3.webp' }
+  { id: 18, img: './NoraStyleSlides/s1.webp' },
+  { id: 19, img: './NoraStyleSlides/s2.webp' },
+  { id: 20, img: './NoraStyleSlides/s3.webp' }
 ];
-export const NoraStyleDescription  = 'ТРЦ Нора  ——  айдентика —— 2021';
+export const NoraStyleDescription = 'ТРЦ Нора  ——  айдентика —— 2021';
