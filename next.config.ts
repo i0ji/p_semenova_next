a313a68b42348b80de023b1eedc75dce5dce29d7
i0ji <ioji@inbox.ru>
2024-11-21 13:08:22 +0300

    yml5

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,12 @@
 import type { NextConfig } from 'next';
 
-//OPTION
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  //OPTION
   output: 'export',
   images: {
     unoptimized: true
   },
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  //OPTION
-  // basePath: isProd ? '/p_katerina_semenova_next' : '',
-  // assetPrefix: isProd ? '/p_katerina_semenova_next' : '',
   basePath: '/p_katerina_semenova_next',
   assetPrefix: '/p_katerina_semenova_next',
   webpack: (config) => {
