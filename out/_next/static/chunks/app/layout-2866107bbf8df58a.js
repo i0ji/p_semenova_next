00e1f118ff495546969c8980a325a3a734b5622c
ioji <ioji@inobx.ru>
2024-11-21 18:50:47 +0300

    18.5

---
+++ out/_next/static/chunks/app/layout-2866107bbf8df58a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{6944:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3449,23))},3449:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[756,755,763,358],()=>s(6944)),_N_E=e.O()}]);
