25b437f30a2e0b3bd0be66bb74b2d584709877ec
i0ji <ioji@inbox.ru>
2024-12-10 14:08:41 +0300

    preprod settings

---
+++ out/index.ts
@@ -0,0 +1,35 @@
+import {
+  AnniversarySlides,
+  AnniversaryDescription
+} from './AnniversarySlides/AnniversaryData';
+import {
+  NagaStyleSlides,
+  NagaStyleDescription
+} from './NagaStyleSlides/NagaSlides';
+import {
+  NoraQuizSlides,
+  NoraQuizDescription
+} from './NoraQuizSlides/NoraQuizSlides';
+import {
+  NoraStyleSlides,
+  NoraStyleDescription
+} from './NoraStyleSlides/NoraStyleSlides';
+import {
+  PatternSlides,
+  PatternDescription
+} from './PatternSlides/PatternSlides';
+import {
+  SeagullSlides,
+  SeagullDescription
+} from './SeagullSlides/SeagullSlides';
+import { TsumSlides, TsumDescription } from './TsumSlides/TsumSlides';
+
+export const SlideData = [
+  { slides: AnniversarySlides, description: AnniversaryDescription, id: 1 },
+  { slides: NagaStyleSlides, description: NagaStyleDescription, id: 2 },
+  { slides: NoraQuizSlides, description: NoraQuizDescription, id: 3 },
+  { slides: NoraStyleSlides, description: NoraStyleDescription, id: 4 },
+  { slides: PatternSlides, description: PatternDescription, id: 5 },
+  { slides: SeagullSlides, description: SeagullDescription, id: 6 },
+  { slides: TsumSlides, description: TsumDescription, id: 7 }
+];
