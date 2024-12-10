9cecec74cd91be98a65cb0091e4b3491ac51e0e5
i0ji <ioji@inbox.ru>
2024-12-10 13:59:44 +0300

    tag

--- out/AnniversarySlides/AnniversaryData.ts
+++
@@ -1,8 +0,0 @@
-export const AnniversarySlides = [
-  { id: 0, img: './AnniversarySlides/a1.webp' },
-  { id: 1, img: './AnniversarySlides/a2.webp' },
-  { id: 2, img: './AnniversarySlides/a3.webp' },
-  { id: 3, img: './AnniversarySlides/a4.webp' }
-];
-
-export const AnniversaryDescription = `Бассейн «Чайка» — оформление к\u00A065—летию бассейна — 2022`;
--- out/NoraStyleSlides/NoraStyleSlides.ts
+++
@@ -1,6 +0,0 @@
-export const NoraStyleSlides = [
-  { id: 18, img: './NoraStyleSlides/s1.webp' },
-  { id: 19, img: './NoraStyleSlides/s2.webp' },
-  { id: 20, img: './NoraStyleSlides/s3.webp' }
-];
-export const NoraStyleDescription = 'ТРЦ Нора — айдентика — 2021';
