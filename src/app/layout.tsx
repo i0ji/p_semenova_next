1416049b0da69cf68147d0ef4c22115a0c3ab08f
i0ji <ioji@inbox.ru>
2024-12-11 21:23:35 +0300

    fix: font/favico path issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,8 @@
 import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+const CenturyGothic = localFont({
+  src: '../../public/centurygothic.woff'
+});
 
 import './globals.scss';
 
@@ -6,13 +10,16 @@ export const metadata: Metadata = {
   title: 'Katerina Semenova - graphic designer'
 };
 
+console.log('v: 0.1.6 a');
+console.log('font/favico path issues');
+
 export default function RootLayout({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ru">
+    <html lang="ru" className={CenturyGothic.className}>
       <body>{children}</body>
     </html>
   );
