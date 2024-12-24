4389cdf52b7db00172418df5164de0f01a3df3da
i0ji <ioji@inbox.ru>
2024-12-24 15:36:09 +0300

    feature: refuse to use one more slider lib due to the inconvenience of the slide

---
+++ src/components/Skeleton/Skeleton.tsx
@@ -0,0 +1,19 @@
+// import React from "react"
+// import ContentLoader from "react-content-loader"
+
+// const MyLoader = (props) => (
+//   <ContentLoader 
+//     speed={2}
+//     width={400}
+//     height={160}
+//     viewBox="0 0 400 160"
+//     backgroundColor="#bdbdbd"
+//     foregroundColor="#8c8c8c"
+//     {...props}
+//   >
+//     <rect x="-26" y="146" rx="3" ry="3" width="217" height="10" /> 
+//     <rect x="-21" y="-50" rx="0" ry="0" width="425" height="182" />
+//   </ContentLoader>
+// )
+
+// export default MyLoader
--- src/components/Slides/Slides.tsx
+++ src/components/Slides/Slides.tsx
@@ -1,100 +1,46 @@
 'use client';
 
-// import { useState, useEffect } from 'react';
-import Carousel from 'react-multi-carousel';
 import Image from 'next/image';
 import styles from './Slides.module.scss';
-// import Skeleton from 'react-loading-skeleton';
-import 'react-multi-carousel/lib/styles.css';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import { useState } from 'react';
 
 export default function Slides(props: SlideModelNamespace.SlidesDataModel) {
-  // const [isLoaded, setIsLoaded] = useState(false);
+  const [loadedImages, setLoadedImages] = useState<Set<number>>(new Set());
 
-  // useEffect(() => {
-  //   if (typeof props.slides[0].img === 'string') {
-  //     const img = new globalThis.Image();
-  //     img.src = props.slides[0].img;
-  //     img.onload = () => setIsLoaded(true);
-  //   }
-  //   return () => {};
-  // }, [props.slides]);
-
-  const responsive = {
-    superLargeDesktop: {
-      breakpoint: { max: 4000, min: 3000 },
-      items: 1
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 1
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 1
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
+  const handleImageLoad = (id: number) => {
+    setLoadedImages((prev) => new Set([...prev, id]));
   };
 
-  // const slideSettings = {
-  //   swipeable: false,
-  //   draggable: false,
-  //   showDots: true,
-  //   responsive: responsive,
-  //   ssr: true,
-  //   infinite: true,
-  //   // autoPlay={this.props.deviceType !== "mobile" ? true : false}
-  //   autoPlaySpeed: 1000,
-  //   keyBoardControl: true,
-  //   // customTransition="all .5"
-  //   transitionDuration: 500,
-  //   containerClass: 'carousel-container',
-  //   // removeArrowOnDeviceType:{["tablet", "mobile"]},
-  //   // deviceType=this.props.deviceType,
-  //   dotListClass: 'custom-dot-list-style',
-  //   itemClass: 'carousel-item-padding-40-px'
-  // };
-
-  const slideSettings = {
-    autoPlay: true,
-    showDots: true,
-    ssr: true,
-    infinite: true
-  };
+  const isAllImagesLoaded = loadedImages.size === props.slides.length;
 
   return (
     <section className={styles.slides}>
-      <Carousel {...slideSettings} responsive={responsive}>
-        {props.slides.map((slide) => (
-          <div key={slide.id} className={styles.slide}>
-            <Image
-              id="image"
-              style={{ padding: '0 10' }}
-              src={slide.img}
-              alt={props.description}
-              className={styles.slide__image}
-              width={1600}
-              height={900}
-            />
-          </div>
-        ))}
-      </Carousel>
-      <p>{props.description}</p>
+      {props.slides.map((slide) => (
+        <div key={slide.id} className={styles.slide}>
+          {!loadedImages.has(slide.id) && (
+            <div className={styles.skeletonWrapper}>
+              <Skeleton height={900} width={1600} />
+            </div>
+          )}
+          <Image
+            id="image"
+            src={slide.img}
+            alt={props.description}
+            className={`${styles.slide__image} ${
+              loadedImages.has(slide.id)
+                ? styles.imageVisible
+                : styles.imageHidden
+            }`}
+            width={1600}
+            height={900}
+            onLoad={() => handleImageLoad(slide.id)}
+          />
+        </div>
+      ))}
+
+      <p>{isAllImagesLoaded ? props.description : <Skeleton count={1} />}</p>
     </section>
   );
 }
-
-// {/* {isLoaded && (
-//   <>
-//     <button
-//       className={styles.slide__leftArrow}
-//       onClick={() => sliderRef.current?.slickPrev()}
-//     />
-//     <button
-//       className={styles.slide__rightArrow}
-//       onClick={() => sliderRef.current?.slickNext()}
-//     />
-//   </>
-// )} */}
