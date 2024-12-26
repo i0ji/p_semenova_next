f29d06f2eb5b0c102414447183d0792789e77c6f
i0ji <ioji@inbox.ru>
2024-12-26 13:41:10 +0300

    issue: slides do not appear

--- src/components/Slides/Slides.tsx
+++ src/components/Slides/Slides.tsx
@@ -2,26 +2,14 @@
 
 import { useRef, useState, useEffect } from 'react';
 
-import Skeleton from 'react-loading-skeleton';
 import Image from 'next/image';
 import Slider from 'react-slick';
 
-import './slick.scss';
+import './Slick.scss';
 import styles from './Slides.module.scss';
 
 export default function Slides(props: SlideModelNamespace.SlidesDataModel) {
   const sliderRef = useRef<Slider>(null);
-
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    if (typeof props.slides[0].img === 'string') {
-      const img = new globalThis.Image();
-      img.src = props.slides[0].img;
-      img.onload = () => setIsLoaded(true);
-    }
-    return () => {};
-  }, [props.slides]);
 
   const settings = {
     dots: true,
@@ -41,35 +29,24 @@ export default function Slides(props: SlideModelNamespace.SlidesDataModel) {
       <Slider ref={sliderRef} {...settings}>
         {props.slides.map((slide) => (
           <div key={slide.id} className={styles.slide}>
-            {isLoaded ? (
-              <Image
-                id="image"
-                src={slide.img}
-                alt={props.description}
-                className={styles.slide__image}
-                width={1600}
-                height={900}
-                priority
-              />
-            ) : (
-              <Skeleton
-                height={900}
-                width={'100%'}
-                style={{ borderRadius: 5 }}
-              />
-            )}
-            {isLoaded && (
-              <>
-                <button
-                  className={styles.slide__leftArrow}
-                  onClick={() => sliderRef.current?.slickPrev()}
-                />
-                <button
-                  className={styles.slide__rightArrow}
-                  onClick={() => sliderRef.current?.slickNext()}
-                />
-              </>
-            )}
+            <Image
+              id="image"
+              src={slide.img}
+              alt={props.description}
+              className={styles.slide__image}
+              width={1600}
+              height={900}
+              priority
+            />
+
+            <button
+              className={styles.slide__leftArrow}
+              onClick={() => sliderRef.current?.slickPrev()}
+            />
+            <button
+              className={styles.slide__rightArrow}
+              onClick={() => sliderRef.current?.slickNext()}
+            />
           </div>
         ))}
       </Slider>
